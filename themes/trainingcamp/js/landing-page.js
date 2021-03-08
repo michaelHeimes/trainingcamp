bcd9d1bad9b082322877a31ff98247b80ef300d3
Michael Heimes <michael@cairndigitalmedia.com>
2021-03-08 17:53:23 -0500

    js cleanup

--- themes/trainingcamp/js/landing-page.js
+++ themes/trainingcamp/js/landing-page.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 
-/*
-	$( ".faq-accordion" ).accordion({
-		collapsible: true,
-		active : 'none'
-	});
-*/
-
 	if ( $('.faq-accordion').length ) {
 
 		$('.faq-accordion .single-faq .answer').slideUp(0);
@@ -49,29 +42,6 @@ $(document).ready(function(){
 		}
 			
 	});
-
-/*
-	$(window).on("load resize", function() {	
-		var $window = $(window);
-		var windowsize = $window.width();
-        
-        if (windowsize > 768) {
-			// Get an array of all element heights
-			var elementHeights = $('.cards-wrap .single-card .inner .top').map(function() {
-				return $(this).height();
-			}).get();
-			
-			// Math.max takes a variable number of arguments
-			// `apply` is equivalent to passing each height as an argument
-			var maxHeight = Math.max.apply(null, elementHeights);
-			
-			// Set each height to the max height
-			$('.cards-wrap .single-card .inner .top').height(maxHeight);
-		}
-			
-	});
-*/
-
 	
 	$('.custom-landing-page.s1 form p.form-row-wide:nth-of-type(1)').addClass('left-side');
 	$('.custom-landing-page.s1 form p.form-row-wide:nth-of-type(2)').addClass('left-side');
